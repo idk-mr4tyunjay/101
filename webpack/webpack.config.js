80dfaf94392267090351105e3c73d9a5cbfae3a7
Mruthunjay <idk.mratyunjay@gmail.com>
2023-12-12 23:00:00 +0530

    css-loader

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,10 +1,18 @@
 const path = require("path");
 module.exports = {
   mode: "development",
-//   devtool: "none",
+  //   devtool: "none",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
-    path: path.resolve(__dirname, "dist")
-  }
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
