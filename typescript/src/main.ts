4f381448057de4a7c51401c332b38ccb74a81aaa
Mruthunjay <idk.mratyunjay@gmail.com>
2023-12-18 22:30:00 +0530

    type-inference

--- typescript/src/main.ts
+++ typescript/src/main.ts
@@ -1,4 +1,22 @@
+//tsc main.ts
 let user = {name: "anand", age: 10}
 
 console.log(user.name);
+
+let greetings: string = "Hello world"
+let mynum = 6;
+greetings.toUpperCase()
+
+console.log(greetings);
+
+
+//number
+let userId: number = 334455
+userId.toFixed()
+
+//typescript automatically assigns as well
+
+//boolean
+let isLoggedIn: boolean = false;
+
 
---
+++ webpack/webpack.dev.js
@@ -0,0 +1,4 @@
+const path = require("path");
+const common = require("./webpack.common");
+const { merge } = require('webpack-merge');
+
